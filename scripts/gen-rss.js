04e55896c040fd06df8d50e54de40f694371a79e
Replit user <>
2024-12-12 16:31:46 +0000

    update contents in rss gen script

--- scripts/gen-rss.js
+++ scripts/gen-rss.js
@@ -5,9 +5,9 @@ const matter = require('gray-matter')
 
 async function generate() {
   const feed = new RSS({
-    title: 'Your Name',
-    site_url: 'https://yoursite.com',
-    feed_url: 'https://yoursite.com/feed.xml'
+    title: 'Søren Rood',
+    site_url: 'https://sorenrood.com',
+    feed_url: 'https://sorenrood.com/feed.xml'
   })
 
   const posts = await fs.readdir(path.join(__dirname, '..', 'pages', 'posts'))
