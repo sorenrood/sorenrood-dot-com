7c1d6008d36fb3a5771b554b54d4022a7aad0700
Replit user <>
2024-12-20 16:59:37 +0000

    colors

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "2c273d8094d5da67"; }
+/******/ 		__webpack_require__.h = function() { return "93dc0d9be1f72948"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
